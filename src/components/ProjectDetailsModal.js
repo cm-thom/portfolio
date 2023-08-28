90f8543cb55b970ee8f486460eb5365c3526a93f
Cole <122782745+cm-thom@users.noreply.github.com>
2023-08-28 15:41:44 -0400

    stripped out images in modals, multiple lang support.

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Modal } from "react-bootstrap";
-import AwesomeSlider from "react-awesome-slider";
-import AwesomeSliderStyles from "../scss/light-slider.scss";
-import AwesomeSliderStyles2 from "../scss/dark-slider.scss";
-import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
+// import AwesomeSlider from "react-awesome-slider";
+// import AwesomeSliderStyles from "../scss/light-slider.scss";
+// import AwesomeSliderStyles2 from "../scss/dark-slider.scss";
+// import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
 class ProjectDetailsModal extends Component {
   render() {
     if (this.props.data) {
       const technologies = this.props.data.technologies;
-      const images = this.props.data.images;
+      // const images = this.props.data.images;
       var title = this.props.data.title;
       var description = this.props.data.description;
       var url = this.props.data.url;
@@ -28,11 +28,11 @@ class ProjectDetailsModal extends Component {
             </li>
           );
         });
-        if (this.props.data.images) {
-          var img = images.map((elem, i) => {
-            return <div key={i} data-src={elem} />;
-          });
-        }
+        // if (this.props.data.images) {
+        //   var img = images.map((elem, i) => {
+        //     return <div key={i} data-src={elem} />;
+        //   });
+        // }
       }
     }
     return (
@@ -47,8 +47,8 @@ class ProjectDetailsModal extends Component {
           <i className="fas fa-times fa-3x close-icon"></i>
         </span>
         <div className="col-md-12">
-          <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
-            <div className="slider-tab">
+          {/* <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}> */}
+            {/* <div className="slider-tab">
               <span
                 className="iconify slider-iconfiy"
                 data-icon="emojione:red-circle"
@@ -67,15 +67,8 @@ class ProjectDetailsModal extends Component {
                 data-icon="twemoji:green-circle"
                 data-inline="false"
               ></span>
-            </div>
-            <AwesomeSlider
-              cssModule={[AwesomeSliderStyles, AwesomeSliderStyles2]}
-              animation="scaleOutAnimation"
-              className="slider-image"
-            >
-              {img}
-            </AwesomeSlider>
-          </div>
+            </div> */}
+          {/* </div> */}
           <div className="col-md-10 mx-auto">
             <h3 style={{ padding: "5px 5px 0 5px" }}>
               {title}
